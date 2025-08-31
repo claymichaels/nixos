405ce18fede0c7b363bc1dab9f871191eb3124ae
Clay Michaels <clay.michaels@gmail.com>
2025-08-30 21:01:05 -0400

    added openssh to pkgs because git is complaining

--- configuration.nix
+++ configuration.nix
@@ -66,6 +66,7 @@
     # Terminal stuff
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     nix-search-cli
+    openssh
     # Hyprland "taskbar"
     waybar
     # notifications
