6cba808a0ea712d34d21fbdaf503599ea08da375
Clay Michaels <clay.michaels@gmail.com>
2026-01-28 18:56:29 -0500

    Add envvar to require venv for PIP

--- configuration.nix
+++ configuration.nix
@@ -108,6 +108,10 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 
+  environment.variables = {
+    PIP_REQUIRE_VIRTUALENV = "true";
+  };
+
   programs.git = {
     enable = true;
     config = {
