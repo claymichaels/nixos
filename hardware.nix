6fb40393c32f9a2b1e45418ca68f40719f2c20f9
Clay Michaels <clay.michaels@gmail.com>
2026-08-28 10:06:14 -0400

    Modified Nvidia settings trying to fix graphical glitches with Pascal card

--- hardware.nix
+++ hardware.nix
@@ -3,17 +3,29 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
   hardware = {
-    # Renamed from opengl.enable
-    graphics.enable = true;
-    # Most Wayland compositors need this
-    nvidia.modesetting.enable = true;
-    nvidia.powerManagement.enable = false;
+    # From https://search.nixos.org/options?channel=26.05&query=graphics.enable&type=options#show=option%253Ahardware.graphics.enable
+    # This option should be enabled by default by the corresponding modules, so you do not usually have to set it yourself.
+    # Whether to enable hardware accelerated graphics drivers
+    # graphics.enable = true;
+
+    # From https://github.com/NixOS/nixos-hardware/blob/master/common/gpu/nvidia/pascal/default.nix
     nvidia.open = false;
-    nvidia.nvidiaSettings = true;
+    nvidia.package = lib.mkDefault config.boot.kernelPackages.legacy_580;
+
+    # Whether to enable nvidia-settings, NVIDIA’s GUI configuration tool. I have this enabled but it doesn't seem to do anything.
+    # nvidia.nvidiaSettings = true;
+
+    # https://wiki.nixos.org/wiki/NVIDIA
+    # Wayland requires kernel mode setting (KMS) to be enabled (Highly Recommended)
+    nvidia.modesetting.enable = true;
+    # Graphical corruption and system crashes on suspend/resume
+    # powerManagement.enable = true can sometimes fix this, but is itself unstable and is known to cause suspend issues
+    nvidia.powerManagement.enable = false;
   };
 
   # Printing
